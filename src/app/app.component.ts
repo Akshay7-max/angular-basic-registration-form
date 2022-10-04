a4d3faa6c305aaa8352dde2a2143aca02cd6da98
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-04 20:14:11 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project2';
+  
+  username: string = "";
+  password: string = "";
+  confpassword: string = "";
+  acceptlicenseagreement: boolean = true;
+  gender: string = "";
+  country: string = "";
+  msg: string = "";
+
+  RegisterClick() {
+    this.msg = "Username : "+this.username+"<br/>Password : "+this.password+"<br />Confirm Password : "+this.confpassword+"<br/>Accept License Agreement : "+this.acceptlicenseagreement+"<br/>Gender : "+this.gender
+    +"<br/>Country : "+this.country;
+  }
 }
